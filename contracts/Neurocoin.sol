bd500457ea4351e532c7174607d012f522d883e2
Dr. Maxim Orlovsky <dr.orlovsky@gmail.com>
2017-05-28 10:40:19 +0300

    ERC20 coin basics

--- contracts/Neurocoin.sol
+++ contracts/Neurocoin.sol
@@ -3,4 +3,13 @@ pragma solidity ^0.4.8;
 import 'zeppelin-solidity/contracts/token/MintableToken.sol';
 
 contract Neurocoin is MintableToken {
+    string public name = "Neurochain Network Token";
+    string public symbol = "NNT";
+    uint public decimals = 18;
+    uint public INITIAL_SUPPLY = 10000;
+
+    function Neurocoin() {
+        totalSupply = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+    }
 }
