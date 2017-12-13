39bcf3d2fdbd3977deada0a3ec65c90bd6a5ad82
Dr. Maxim Orlovsky <dr.orlovsky@gmail.com>
2017-12-13 14:23:22 +0200

    Improving migrations

--- migrations/3_binding_factories.js
+++ migrations/3_binding_factories.js
@@ -3,6 +3,11 @@ let CognitiveJobFactory = artifacts.require("CognitiveJobFactory")
 let WorkerNodeFactory = artifacts.require("WorkerNodeFactory")
 
 module.exports = function(deployer, network, accounts) {
-  WorkerNodeFactory.deployed().then(factory => factory.transferOwnership(Pandora.address)).catch(e => console.log(e))
-  CognitiveJobFactory.deployed().then(factory => factory.transferOwnership(Pandora.address)).catch(e => console.log(e))
+  WorkerNodeFactory.deployed()
+  .then(wnf => wnf.transferOwnership(Pandora.address))
+  .then(_ => CognitiveJobFactory.deployed())
+  .then(cjf => cjf.transferOwnership(Pandora.address))
+  .then(_ => Pandora.deployed())
+  .then(pandora => pandora.initialize())
+  .catch(e => console.log(e))
 }
--- migrations/4_init.js
+++
@@ -1,7 +0,0 @@
-let Pandora = artifacts.require("Pandora")
-let CognitiveJobFactory = artifacts.require("CognitiveJobFactory")
-let WorkerNodeFactory = artifacts.require("WorkerNodeFactory")
-
-module.exports = function(deployer, network, accounts) {
-  Pandora.deployed().then(pandora => pandora.initialize()).catch(e => console.log(e))
-}
