ac50bf43d5fd7bcf099c108a06c7ac8a3407be94
Dr. Maxim Orlovsky <orlovsky@pandora.foundation>
2017-12-17 10:20:48 +0200

    Adding automatic deployment of cognitive job contract

--- migrations/3_binding_factories.js
+++ migrations/3_binding_factories.js
@@ -1,6 +1,9 @@
 let Pandora = artifacts.require("PandoraHooks") // *** NB: version with hooks (for testing) instead of normal contract)
 let CognitiveJobFactory = artifacts.require("CognitiveJobFactory")
 let WorkerNodeFactory = artifacts.require("WorkerNodeFactory")
+let WorkerNode = artifacts.require("WorkerNode")
+let Kernel = artifacts.require("Kernel")
+let Dataset = artifacts.require("Dataset")
 
 module.exports = function(deployer, network, accounts) {
   let pandora;
@@ -15,5 +18,12 @@ module.exports = function(deployer, network, accounts) {
     return pandora.initialize()
   })
   .then(_ => pandora.createWorkerNode({ from: accounts[0] }))
+  .then(tx => {
+    const address = tx.logs[1].args['workerNode']
+    console.log("  WorkerNode: " + address)
+    return WorkerNode.at(address)
+  })
+  .then(workerNode => workerNode.alive())
+  .then(_ => pandora.createCognitiveJob(Kernel.address, Dataset.address))
   .catch(e => console.log(e))
 }
