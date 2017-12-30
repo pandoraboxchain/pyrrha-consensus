6aea930bfaef05f62ff8a3419ad12ecad7adc978
Dr. Maxim Orlovsky <orlovsky@pandora.foundation>
2017-12-30 08:19:11 +0100

    Described arguments taken by the Kernel and Dataset constructors

--- contracts/entities/Dataset.sol
+++ contracts/entities/Dataset.sol
@@ -10,12 +10,18 @@ contract Dataset is DataEntity, IDataset {
     uint256 public samplesCount;
     uint8 public batchesCount;
 
+    /// @dev Constructor receives an address of the main IPFS dataset information file (in JSON format) and four
+    /// core arguments (also present in that file) which are required for smartcontracts to be able to initialize
+    /// cognitive jobs and check compliance with the selected kernel
     function Dataset (
-        bytes _ipfsAddress,
-        uint256 _dataDim,
-        uint256 _samplesCount,
-        uint8 _batchesCount,
-        uint256 _initialPrice
+        bytes _ipfsAddress,     /// Address for the JSON file stored in IPFS that keeps all necessary information on
+                                /// the dataset, including IPFS addresses for each batch in HD5F format
+        uint256 _dataDim,       /// Dimension of the single dataset sample (that goes to the input layer of the
+                                /// kernel neural network
+        uint256 _samplesCount,  /// Total amount of samples in dataset (sum of samples in all batches)
+        uint8 _batchesCount,    /// Amount of batches defined in the information JSON file (each batch will be computed
+                                /// by a separate worker node
+        uint256 _initialPrice   /// Price for which Dataset owner is ready to rent the kernel
     )
     DataEntity(_ipfsAddress, _dataDim, _initialPrice)
     public {
--- contracts/entities/Kernel.sol
+++ contracts/entities/Kernel.sol
@@ -15,11 +15,16 @@ import './IKernel.sol';
 contract Kernel is DataEntity, IKernel {
     uint256 public complexity;
 
+    /// @dev Constructor receives an address of the main IPFS kernel info file and three core arguments (also present
+    /// in that file) which are required for smartcontracts to be able to initialize cognitive jobs and check
+    /// compliance with the selected dataset
     function Kernel (
-        bytes _ipfsAddress,
-        uint256 _dataDim,
-        uint256 _complexity,
-        uint256 _initialPrice
+        bytes _ipfsAddress, /// Address for the JSON file stored in IPFS that keeps all necessary information on
+                            /// the kernel, including IPFS addresses for HD5F files with weights and JSON model
+        uint256 _dataDim,   /// Dimension of the input vector for the topmost network layer
+        uint256 _complexity,    /// Amount of computing operations that must be performed for a single data pass
+                                /// throughout neural network to get a single result
+        uint256 _initialPrice   /// Price for which Kernel author is ready to rent the kernel
     )
     DataEntity(_ipfsAddress, _dataDim, _initialPrice)
     public {
