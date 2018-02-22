e50c3215ebd3b40612beb99e9e6397bd56819958
Dr. Maxim Orlovsky <orlovsky@pandora.foundation>
2018-02-22 21:28:21 +0200

    Improving code for the contract deployment and truffle network configuration

--- migrations/2_deploy_factories.js
+++ migrations/2_deploy_factories.js
@@ -1,17 +1,44 @@
-let Pandora = artifacts.require("Pandora")
-let WorkerNode = artifacts.require("WorkerNode")
-let CognitiveJob = artifacts.require("CognitiveJob")
-let CognitiveJobFactory = artifacts.require("CognitiveJobFactory")
-let WorkerNodeFactory = artifacts.require("WorkerNodeFactory")
-let StateMachineLib = artifacts.require("StateMachineLib")
+const Pandora = artifacts.require("Pandora")
+const WorkerNode = artifacts.require("WorkerNode")
+const CognitiveJob = artifacts.require("CognitiveJob")
+const CognitiveJobFactory = artifacts.require("CognitiveJobFactory")
+const WorkerNodeFactory = artifacts.require("WorkerNodeFactory")
+const StateMachineLib = artifacts.require("StateMachineLib")
 
 module.exports = function(deployer, network, accounts) {
-  let workerOwners = [ accounts[0], accounts[0], accounts[0] ]
+  let lastAccount = accounts[0]
+  let workerOwners = [ ]
+  for (let i = 1; i <= 3; i++) {
+    workerOwners.push(lastAccount)
+    if (accounts[i]) {
+      lastAccount = accounts[i]
+    }
+  }
 
-  deployer.deploy(StateMachineLib)
+  let pandora, wnf, cjf
+
+  deployer
+  .then(_ => deployer.deploy(StateMachineLib))
   .then(_ => deployer.link(StateMachineLib, [ WorkerNode, CognitiveJob ]))
-  .then(_ => deployer.deploy(CognitiveJobFactory))
-  .then(_ => deployer.deploy(WorkerNodeFactory))
-  .then(_ => deployer.deploy(Pandora, CognitiveJobFactory.address, WorkerNodeFactory.address, workerOwners))
+  .then(_ => {
+    return deployer.deploy(CognitiveJobFactory)
+  })
+  .then(_ => CognitiveJobFactory.deployed())
+  .then(instance => {
+    cjf = instance
+    return deployer.deploy(WorkerNodeFactory)
+  })
+  .then(_ => WorkerNodeFactory.deployed())
+  .then(instance => {
+    wnf = instance
+    return deployer.deploy(Pandora, cjf.address, wnf.address, workerOwners)
+  })
+  .then(_ => Pandora.deployed())
+  .then(instance => {
+    pandora = instance
+    return wnf.transferOwnership(pandora.address)
+  })
+  .then(_ => cjf.transferOwnership(pandora.address))
+  .then(_ => pandora.initialize())
   .catch(e => console.error(e))
 }
