199f75f0537e0c28af6aef97ac1346155a4a5494
Dr. Maxim Orlovsky <orlovsky@pandora.foundation>
2018-02-22 15:20:29 +0200

    Added deployment to RSK network

--- migrations/7_cognitive_job.js
+++ migrations/7_cognitive_job.js
@@ -1,9 +1,21 @@
 let Pandora = artifacts.require("Pandora") // *** NB: for testing use version with hooks (PandoraHooks) instead of normal contract
 let Kernel = artifacts.require("Kernel")
 let Dataset = artifacts.require("Dataset")
+let WorkerNode = artifacts.require("WorkerNode")
 
 module.exports = function(deployer, network, accounts) {
+  let pandora
+
   Pandora.deployed()
-  //.then(pandora => pandora.createCognitiveJob(Kernel.address, Dataset.address))
+  .then(p => {
+    pandora = p
+    return WorkerNode.deployed()
+  })
+  .then(_ => pandora.createCognitiveJob(Kernel.address, Dataset.address))
+  .then(tx => {
+    console.log(tx)
+//    const address = tx.logs[0].args['cognitiveJob']
+//    console.log("  CognitiveJob: " + address)
+  })
   .catch(e => console.log(e))
 }
--- truffle.js
+++ truffle.js
@@ -1,10 +1,10 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
 
-var HDWalletProvider = require("truffle-hdwallet-provider");
+let HDWalletProvider = require("truffle-hdwallet-provider")
 
-var infura_apikey = "5d45HvcGSajS3BtffliU";
-var mnemonic = "dinner govern better mix core bean illegal rain crash afraid double company";
+let infura_apikey = "5d45HvcGSajS3BtffliU"
+let mnemonic = "dinner govern better mix core bean illegal rain crash afraid double company"
 
 module.exports = {
   networks: {
@@ -15,7 +15,7 @@ module.exports = {
       network_id: '*' // Match any network id
     },
     ganache: {
-      gas: 6700000,
+      gas: 4700000,
       host: 'localhost',
       port: 7545,
       network_id: '5777'
@@ -31,12 +31,23 @@ module.exports = {
       port: 8545,
       network_id: '3' // Match any network id
     },
+    rinkeby: {
+      provider: _ => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/"+infura_apikey),
+      network_id: '*',
+      gas: 4700000
+    },
     testnet: {
       gas: 4700000,
       from: '0x00Ea169ce7e0992960D3BdE6F5D539C955316432',
       host: '52.232.83.9',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    rsktest: {
+      host: 'bitcoin.pandora.network',
+      port: '4444',
+      network_id: '*',
+      gasPrice: 0
     }
   }
 }
