bb1ae4c1bdc202a3cc3c17d2f37d8b391a0cc69c
Koroqe <koroqe@gmail.com>
2018-04-11 15:31:58 +0300

    Add test whitelisting node owner during deployment

---
+++ test/test_worker_whitelisting.js
@@ -0,0 +1,19 @@
+let Pandora = artifacts.require("Pandora")
+
+
+contract('Pandora', accounts => {
+
+  let pandora
+
+  before('setup', async () => {
+    pandora = await Pandora.deployed()
+  })
+
+  it('Account #0 should be whitelisted during deployment', async () => {
+
+    let result = await pandora.workerNodeOwners(accounts[0])
+    console.log(result)
+
+    assert.equal(result, true, "account #0 should be whitelisted")
+  })
+})
